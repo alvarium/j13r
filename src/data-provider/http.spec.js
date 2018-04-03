30f635244705524e6c832ae25ea816c9464db3f5
Genar Trias Ortiz <genar@acs.li>
2018-04-03 19:29:38 +0200

    WIP: Starting to define http data-provider

---
+++ src/data-provider/http.spec.js
@@ -0,0 +1,9 @@
+const http = require('./http')
+
+describe('HTTP Data provider', () => {
+  describe('retrieveData', () => {
+    test('Should fetch data from provided url', (done) => {
+      done()
+    })
+  })
+})
